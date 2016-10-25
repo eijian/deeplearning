8f78e2d9167cd09a0b569fd7cc9c04f7394a71fc
Eiji Akagi <akagi.eiji@gmail.com>
2016-10-26 02:25:29 +0900

    parameters are separated from Main

--- src/Main-train.hs
+++ src/Main-train.hs
@@ -21,71 +21,24 @@ import CNN.PoolLayer
 
 import Pool
 import Trainer
-
--- PARAMETERS
-k = 3   -- number of class
-n = 50  -- number of teacher data for each class
-m = 10  -- number of test data for each class
-
-train_N    = n * k
-test_N     = m * k
-image_size = [12, 12]
-channel    = 1
-
-n_kernels    = [10, 20]
-kernel_sizes = [3, 2]
-pool_sizes   = [2, 2]
-n_hidden     = 20
-n_out = k
-
-epochs = 500
---epochs = 2000
---epochs = 1
-opStep = 5
---opStep = 1
-epoch0 = 1
-batch  = 150
---batch  = 1
-
-learning_rate = 0.1
+import Status
 
 -- MAIN
 
 main :: IO ()
 main = do
-  putStrLn "Building the model..."
-  poolT <- initSamplePool 1 (12, 12) 3 0.95 train_N
-  poolE <- initSamplePool 1 (12, 12) 3 0.90 test_N
-  sampleE <- getImages poolE test_N 1
+  st <- loadStatus ""
+  sampleE <- getImages (poolE st) (ntest st) 1
   tm0 <- getCurrentTime
 
-  fc1 <- initFilterC 10 1 12 12 3 2
-  fc2 <- initFilterC 20 10 5 5 2 2
-  ff1 <- initFilterF n_hidden (2*2*20)
-  --ff2 <- initFilterF n_out n_hidden
-  ff2 <- zeroFilterF n_out n_hidden
-
   let
-    is = reverse $ [epoch0 .. (epoch0 + epochs - 1)]
-    getTeachers = getImages poolT batch
-    putF = putStatus tm0 epochs epoch0 opStep
-    layers = [
-        ConvLayer 3 fc1
-      , ActLayer relu
-      , MaxPoolLayer 2
-      , ConvLayer 2 fc2
-      , ActLayer relu
-      , MaxPoolLayer 2
-      , FlattenLayer 2 2
-      , FullConnLayer ff1
-      , ActLayer relu
-      , FullConnLayer ff2
-      , ActLayer softmax
-      ]
+    is = reverse $ [(epoch0 st) .. ((epoch0 st) + (nepoch st) - 1)]
+    getTeachers = getImages (poolT st) (batch st)
+    putF = putStatus tm0 (nepoch st) (epoch0 st) (opstep st)
 
   putStrLn "Training the model..."
-  putF 0 layers sampleE
-  layers' <- loop getTeachers sampleE putF learning_rate layers is
+  putF 0 (layers st) sampleE
+  layers' <- loop getTeachers sampleE putF (learnR st) (layers st) is
   putStrLn "Finished!"
 
 {- |
---
+++ src/Status.hs
@@ -0,0 +1,121 @@
+--
+-- Status : neural network status
+--
+
+module Status (
+  loadStatus
+, saveStatus
+, layers
+, learnR
+, batch
+, epoch0
+, nepoch
+, opstep
+, poolT
+, poolE
+, ntest
+) where
+
+import CNN.ActLayer
+import CNN.ConvLayer
+import CNN.FullConnLayer
+import CNN.LayerType
+import CNN.PoolLayer
+
+import Pool
+
+data Status = Status {
+    layers :: [Layer]
+  , learnR :: Double
+  , batch  :: Int
+  , epoch0 :: Int
+  , nepoch :: Int
+  , opstep :: Int
+  , poolT  :: MemPool
+  , poolE  :: MemPool
+  , ntest  :: Int
+  }
+
+{-
+loadStatus
+
+  IN : filename
+
+  OUT: loaded/built status
+
+-}
+
+loadStatus :: String -> IO Status
+loadStatus name
+  | name == "" = staticStatus
+  | otherwise  = loadFromFile name
+
+staticStatus :: IO Status
+staticStatus = do
+  let
+    k = 3   -- number of class
+    n = 50  -- number of teacher data for each class
+    m = 10  -- number of test data for each class
+
+    train_N    = n * k
+    test_N     = m * k
+    image_size = [12, 12]
+    channel    = 1
+
+    n_kernels    = [10, 20]
+    kernel_sizes = [3, 2]
+    pool_sizes   = [2, 2]
+    n_hidden     = 20
+    n_out = k
+
+  pt <- initSamplePool 1 (12, 12) 3 0.95 train_N
+  pe <- initSamplePool 1 (12, 12) 3 0.90 test_N
+
+  fc1 <- initFilterC 10 1 12 12 3 2
+  fc2 <- initFilterC 20 10 5 5 2 2
+  ff1 <- initFilterF n_hidden (2*2*20)
+  --ff2 <- initFilterF n_out n_hidden
+  ff2 <- zeroFilterF n_out n_hidden
+
+  let
+    ls = [
+        ConvLayer 3 fc1
+      , ActLayer relu
+      , MaxPoolLayer 2
+      , ConvLayer 2 fc2
+      , ActLayer relu
+      , MaxPoolLayer 2
+      , FlattenLayer 2 2
+      , FullConnLayer ff1
+      , ActLayer relu
+      , FullConnLayer ff2
+      , ActLayer softmax
+      ]
+
+    stat = Status {
+        layers = ls
+      , learnR = 0.1
+      , batch  = 150
+      , epoch0 = 1
+      , nepoch = 500
+      , opstep = 5
+      , poolT  = pt
+      , poolE  = pe
+      , ntest  = m * k
+      }
+
+  return stat
+
+
+loadFromFile :: String -> IO Status
+loadFromFile fname = staticStatus
+
+{-
+saveStatus
+  IN : filename for output
+       status
+       updated layers
+-}
+
+saveStatus :: String -> Status -> [Layer] -> IO ()
+saveStatus fname st ls = putStrLn ("saved to '" ++ fname ++ "'")
