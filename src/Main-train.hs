65507285b044cbc9d2b2e8c3d0a9076c744b0a4e
Eiji Akagi <akagi.eiji@gmail.com>
2017-01-03 20:56:23 +0900

    trainloop to fold version

--- src/Main-train.hs
+++ src/Main-train.hs
@@ -35,17 +35,37 @@ main = do
   tm0 <- getCurrentTime
 
   let
-    is = [(epoch_ed st), (epoch_ed st - 1) .. (epoch_st st)]
+    --is = [(epoch_ed st), (epoch_ed st - 1) .. (epoch_st st)]
+    is = [(epoch_st st) .. (epoch_ed st)]
     getTeachers = getImages (poolT st) (batch st)
     putF = putStatus tm0 (epoch_st st) (epoch_ed st) (opstep st)
+    loopFunc = trainLoop' getTeachers sampleE putF (learnR st)
 
   putStrLn "Training the model..."
   putF 0 (layers st) sampleE
-  layers' <- trainLoop getTeachers sampleE putF (learnR st) (layers st) is
+  --layers' <- trainLoop getTeachers sampleE putF (learnR st) (layers st) is
+  layers' <- foldM' loopFunc (layers st) is
 
   putStrLn "Saving status..."
   saveStatus "" st layers'
   putStrLn "Finished!"
+
+--
+-- FUNCTIONS
+--
+
+{- |
+foldM' : monadic version of foldl'
+
+  URL: http://stackoverflow.com/questions/8919026/does-haskell-have-foldlm
+
+-}
+
+foldM' :: (Monad m) => (a -> b -> m a) -> a -> [b] -> m a
+foldM' _ z [] = return z
+foldM' f z (x:xs) = do
+  z' <- f z x
+  z' `seq` foldM' f z' xs
 
 {- |
 trainLoop
@@ -74,6 +94,25 @@ trainLoop getT se putF lr ls (i:is) = do
     ls'' = update lr (transpose dls) ls'           -- dls = diff of layers
   putF i ls'' se
   return ls''
+
+trainLoop' :: (Int -> IO [Trainer]) -> [Trainer]
+  -> (Int -> [Layer] -> [Trainer] -> IO ()) -> Double -> [Layer] -> Int
+  -> IO [Layer]
+trainLoop' getT se putF lr ls i = do
+  teachers <- getT i
+  let ls' = updateLayers lr teachers ls
+  putF i ls' se
+  return ls'
+
+{- |
+updateLayers
+-}
+
+updateLayers :: Double -> [Trainer] -> [Layer] -> [Layer]
+updateLayers lr ts ls = update lr (transpose dls) ls
+  where
+    rls = tail $ map reverseLayer $ reverse ls    -- fist element isn't used
+    dls = map (train ls rls) ts             -- dls = diff of layers
 
 {-
 putStatus
