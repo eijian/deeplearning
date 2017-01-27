91697b1a45e7305b8fbf7e588a1f7142ddab5c96
Eiji Akagi <akagi.eiji@gmail.com>
2017-01-28 02:13:50 +0900

    update

--- CNN/FlattenLayer.hs
+++ CNN/FlattenLayer.hs
@@ -7,40 +7,49 @@ module CNN.FlattenLayer (
 , unflatten
 ) where
 
+import qualified Numeric.LinearAlgebra as A
+
 import CNN.Image
 import CNN.LayerType
 
 {- |
 flatten: flatten image
 
-  IN: Image (=[[[Double]]])
+  IN: Image (=[Matrix R])
 
->>> let im = [[[2.0,3.0],[4.0,5.0]],[[1.0,2.0],[4.0,3.0]],[[2.0,1.0],[6.0,5.0]]]
+>>> let im = [A.fromLists [[2.0,3.0],[4.0,5.0]], A.fromLists [[1.0,2.0],[4.0,3.0]], A.fromLists [[2.0,1.0],[6.0,5.0]]]
 >>> flatten im
-[[[2.0,3.0,4.0,5.0,1.0,2.0,4.0,3.0,2.0,1.0,6.0,5.0]]]
+[(1><12)
+ [ 2.0, 3.0, 4.0, 5.0, 1.0, 2.0, 4.0, 3.0, 2.0, 1.0, 6.0, 5.0 ]]
 
 -}
 
 flatten :: Image -> Image
-flatten im = [[concat $ concat im]]
+flatten im = [A.fromLists [concat $ map (A.toList . A.flatten) im]]
 
 {- |
 unflatten: unflatten image
 
-  IN: [[[Double]]]
+  IN: [Matrix R]
 
->>> let ds = [[[2.0,3.0,4.0,5.0,1.0,2.0,4.0,3.0,2.0,1.0,6.0,5.0]]]
+>>> let ds = [A.fromLists [[2.0,3.0,4.0,5.0,1.0,2.0,4.0,3.0,2.0,1.0,6.0,5.0]]]
 >>> unflatten 2 2 ds
-[[[2.0,3.0],[4.0,5.0]],[[1.0,2.0],[4.0,3.0]],[[2.0,1.0],[6.0,5.0]]]
+[(2><2)
+ [ 2.0, 3.0
+ , 4.0, 5.0 ],(2><2)
+ [ 1.0, 2.0
+ , 4.0, 3.0 ],(2><2)
+ [ 2.0, 1.0
+ , 6.0, 5.0 ]]
 
 -}
 
 unflatten :: Int -> Int -> Image -> Image
-unflatten x y [[ds]] = split y $ split x ds
+unflatten x y [m] = map (A.reshape x . A.fromList) $ split (y*x) v
   where
+    v = A.toList $ A.flatten m
     split :: Int -> [a] -> [[a]]
     split _ [] = []
     split n ds = d : split n ds'
       where
         (d, ds') = splitAt n ds
-
